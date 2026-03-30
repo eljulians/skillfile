ad7bc7cc6ab21d3c403ad5f2ff0ae8145e9414eb
Rohit Sharma <rohit.sharma@archonlabs.com>
2026-03-30 11:41:15 +0200

    Add flake.

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Skillfile development environment";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = false;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.rustc
+            pkgs.cargo
+            pkgs.rustfmt
+            pkgs.clippy
+          pkgs.git
+        ];
+        };
+      }
+    );
+}
